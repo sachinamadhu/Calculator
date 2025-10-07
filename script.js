e79a1fe1f05f64d5aa8d9c0bbae5284b42c99777
sachinamadhu <132274125+sachinamadhu@users.noreply.github.com>
2025-10-07 14:48:43 +0530

    Add calculator files

---
+++ script.js
@@ -0,0 +1,21 @@
+function appendResult(val) {
+    document.getElementById("result").value += val;
+}
+
+function clearResult() {
+    document.getElementById("result").value = "";
+}
+
+function deleteLast() {
+    let result = document.getElementById("result").value;
+    document.getElementById("result").value = result.slice(0, -1);
+}
+
+function calculateResult() {
+    let result = document.getElementById("result").value;
+    try {
+        document.getElementById("result").value = eval(result);
+    } catch (e) {
+        document.getElementById("result").value = "Error";
+    }
+}
